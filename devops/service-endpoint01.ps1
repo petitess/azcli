29952ba798ba4d1353cf4cd8dcaf090780584c73
Karol <93782725+petitess@users.noreply.github.com>
2023-10-10 10:00:34 +0200

    get service connection

---
+++ devops/service-endpoint01.ps1
@@ -0,0 +1,10 @@
+$devopsOrg = "https://dev.azure.com/xxx"
+$project = "Infrastruktur"
+$spiname = "sp-labb-01x"
+$endpointId = az devops service-endpoint list --org $devopsOrg --project $project --query "[?name=='$spiname'].id" -o tsv 
+
+if ($endpoint) {
+    Write-Output "Found: $endpointId"
+}else{
+    Write-Output "Not Found"
+}
