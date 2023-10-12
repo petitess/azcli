9838feafbe365f06a9a96de20c9f68eb61b07f7e
Karol <93782725+petitess@users.noreply.github.com>
2023-10-12 16:48:55 +0200

    Get app registration id

--- app/app01.ps1
+++ app/app01.ps1
@@ -1,7 +1,7 @@
 $spiname = "sp-serviceaccess-prod-01"
 $appId = az ad app list --query "[?displayName=='$spiname'].id" --all -o tsv
 
-if ($app) {
+if ($appId) {
     Write-Output "Found: $appId"
 }else{
     Write-Output "Not Found"
