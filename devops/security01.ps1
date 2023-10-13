bd7ca0adf42b1fa2b55683811b2e965fecc2c383
Karol <93782725+petitess@users.noreply.github.com>
2023-10-13 14:18:16 +0200

    Add a user to a security group

---
+++ devops/security01.ps1
@@ -0,0 +1,8 @@
+#az devops user list	| Out-File users.json
+
+#az devops security group list --scope organization | Out-File groups.json
+
+$groupDescriptor = "vssgp.Uy0xLTktMTU1MTM3NDI0NS0xODU1OTU2NTEyLTEwMzE5MzMyNTgtMjQ0MjI0OTg1OS03OTxxxxxxxxxxxxx"
+$userDescriptor = "aad.YzY2MTQzNTEtM2U5MS03MzUxLWE3MjItOTE5NWExxx"
+
+az devops security group membership add --org $devopsOrg --group-id $groupDescriptor --member-id $userDescriptor
