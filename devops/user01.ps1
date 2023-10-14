e3980e18eada9bd77ea4c363c7e7df1890177557
Karol <93782725+petitess@users.noreply.github.com>
2023-10-14 14:28:42 +0200

    List devops user properties

--- devops/user01.ps1
+++ devops/user01.ps1
@@ -2,4 +2,5 @@ az devops user list --query "items" -o tsv
 az devops user list --query "items[].user.descriptor" -o tsv
 az devops user list --query "items[].user.principalName" -o tsv
 az devops user list --query "items[].{principalName:user.principalName, descriptor:user.descriptor}" | Out-File groups.json
+az devops user list --query "items[].{principalName:user.principalName, descriptor:user.descriptor}" --output table
 az devops user list --query "items[?user.principalName=='kar.sek.ext@xxx.se'].user.descriptor" -o tsv
