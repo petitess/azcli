08243643d62266c9fd5836c9fc5dc11cee2c8003
Karol <93782725+petitess@users.noreply.github.com>
2023-10-24 11:37:02 +0200

    get subscription info

--- account/account01.ps1
+++ account/account01.ps1
@@ -2,3 +2,4 @@ az account list
 az account list --query "[].name" -o tsv
 az account list --query "[?name=='sub-infra-dev-01']"
 az account list --query "[?name=='sub-infra-dev-01'].id" -o tsv
+az account list --query "[?isDefault]"
