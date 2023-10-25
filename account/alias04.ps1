9c47d894a5223cde0db867332207f22d3547dba7
Karol <93782725+petitess@users.noreply.github.com>
2023-10-25 16:51:30 +0200

    create sub alias + service connection + federated credentials v2

--- account/alias04.ps1
+++ account/alias04.ps1
@@ -8,14 +8,14 @@ param (
 
 $billingAccounts = 'xxxx-8228-43a1-acf8-32cb5b4c5d92:b5b6fef4-e666-4fd2-b303-xxxx'
 $billingProfile = 'xxx-U5HS-BG7-xxx'
-$invoiceSections = 'xxxx-QLVW-PJA-xxx'
+$invoiceSections = 'xxx-QLVW-PJA-xxx'
 $billingScope = "/providers/Microsoft.Billing/billingAccounts/$billingAccounts/billingProfiles/$billingProfile/invoiceSections/$invoiceSections"
 $rbac = "Owner"
-$devopsOrg = "https://dev.azure.com/xxxx"
-$devopsOrgName = "xxx"
+$devopsOrg = "https://dev.azure.com/xxx"
+$devopsOrgName = "xxxx"
 $projectId = az devops project show --project $devopsProjectName --query "id" --output tsv
 $tenantID = az account show --query "tenantId" -o tsv
-$issuer = "https://vstoken.dev.azure.com/xxxx-823d-4a0d-9191-xxxxx"
+$issuer = "https://vstoken.dev.azure.com/xxxx-823d-4a0d-9191-xxx"
 $token = az account get-access-token --query accessToken --output tsv
 
 #Create subscriptions
@@ -29,7 +29,7 @@ else {
 $spiname = ($sub).Replace('sub', 'sp')
 
 # Create Service Principal
-$spipasswd = az ad sp create-for-rbac -n $spiname --query "password" -o tsv
+az ad sp create-for-rbac -n $spiname --query "id" -o tsv
 
 # Query the Application ID of the Service Principal and Store it in a variable:-
 $spiID = az ad sp list --display-name $spiname --query [].appId -o tsv
@@ -39,12 +39,12 @@ $newSub = az account alias show --name $sub --query "properties.subscriptionId" 
 az role assignment create --assignee "$spiID" --role "$rbac" --scope "/subscriptions/$newSub" -o table
     
 #Set Service Principal Secret as an Environment Variable for creating Azure DevOps Service Connection:-
-$env:AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY = $spipasswd
+#$env:AZURE_DEVOPS_EXT_AZURE_RM_SERVICE_PRINCIPAL_KEY = $spipasswd
 
 # Set Default DevOps Organisation and Project:-
 az devops configure --defaults organization=$devopsOrg project=$devopsProjectName -o table
 
-# Create DevOps Service Connection with Federated Credentials:-
+# Create DevOps Service Connection:-
 #az devops service-endpoint azurerm create --azure-rm-service-principal-id $spiID --azure-rm-subscription-id $newSub --azure-rm-subscription-name $sub --azure-rm-tenant-id $tenantID --name $spiname --org $devopsOrg --project $devopsProjectName -o table
 
 #Add subscriptions to management group
@@ -66,31 +66,30 @@ else {
 
 # Create DevOps Service Connection with Federated Credentials:-
 $serviceEndpointId = az devops service-endpoint list --query "[?name=='$spiname'].id" -o tsv
-
+$authorization = az devops service-endpoint list --query "[?name=='$spiname'].authorization.scheme" -o tsv
 if ($serviceEndpointId) {
-    Write-Output "Service endpoint already exists. Update manually."
-    # Write-Output "Updating service endpoint"
-    # $uri = "https://dev.azure.com/$devopsOrgName/$devopsProjectName/_apis/serviceendpoint/endpoints/${serviceEndpointId}?operation=ConvertAuthenticationScheme&api-version=7.2-preview.4"
-    # $body = ConvertTo-Json -Depth 10 @{
-    #     type                             = "azurerm"
-    #     authorization                    = @{
-    #         scheme = "WorkloadIdentityFederation"
-    #         #scheme = "ServicePrincipal"
-    #     }
-    #     serviceEndpointProjectReferences = @(
-    #         @{
-    #             description      = ""
-    #             name             = $spiname
-    #             projectReference = @{
-    #                 id   = $projectId
-    #                 name = $devopsProjectName
-    #             }
-    #         }
-    #     )
-    # }
-    
-    # Invoke-RestMethod  -Method PUT -Uri $uri  -Headers @{ Authorization = "Bearer $token"; "Content-Type" = "application/json" } -Body $body
-
+    Write-Output "Found service endpoint: $serviceEndpointId"
+    if ($authorization -notmatch "WorkloadIdentityFederation") {
+        Write-Output "Configuring Workload Identity Federation"
+        $uri = "https://dev.azure.com/$devopsOrgName/$devopsProjectName/_apis/serviceendpoint/endpoints/${serviceEndpointId}?operation=ConvertAuthenticationScheme&api-version=7.2-preview.4"
+        $body = ConvertTo-Json -Depth 10 @{
+            type                             = "azurerm"
+            authorization                    = @{
+                scheme = "WorkloadIdentityFederation"
+            }
+            serviceEndpointProjectReferences = @(
+                @{
+                    description      = ""
+                    name             = $spiname
+                    projectReference = @{
+                        id   = $projectId
+                        name = $devopsProjectName
+                    }
+                }
+            )
+        }
+        Invoke-RestMethod  -Method PUT -Uri $uri  -Headers @{ Authorization = "Bearer $token"; "Content-Type" = "application/json" } -Body $body
+    }
 }
 else {
     Write-Output "Creating service endpoint"
