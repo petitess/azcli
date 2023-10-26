516fd5e56b8dadda5430a2395c184daf3c052355
Karol <93782725+petitess@users.noreply.github.com>
2023-10-26 15:13:13 +0200

    create pipelines with build validation policy

--- pipelines/pipelines02.ps1
+++ pipelines/pipelines02.ps1
@@ -13,7 +13,7 @@ foreach ($Product in $Products) {
     }
 }
 
-foreach ($Product in $Products) {
+foreach ($Product in $Products | Where-Object { $_ -notmatch "bicepconfig.json" } ) {
     $repoName = 'ssg-landingzones'
     $repoId = az repos list --query "[?name=='$repoName'].id" -o tsv
     $pipelineName = "$Product-$System - CI"
@@ -21,7 +21,7 @@ foreach ($Product in $Products) {
     $policyValidation = az repos policy list --query "[?settings.displayName=='$pipelineName']" -o tsv
 
     if ($null -eq $policyValidation) {
-        Write-Output "Creating Build Validation policy. $pipelineName."
+        Write-Output "Creating Build Validation policy: $pipelineName."
         az repos policy build create --blocking true `
             --branch "refs/heads/main" `
             --build-definition-id $buildId `
@@ -31,9 +31,9 @@ foreach ($Product in $Products) {
             --queue-on-source-update-only false `
             --repository-id $repoId `
             --valid-duration "0.0" `
-            --path-filter "/ci/ci.yml;/iac/*"
+            --path-filter "/landingzones/$Product/ci.yml;/iac/*"
     }
     else {
-        Write-Output "Build Validation policy already exists. $pipelineName."
+        Write-Output "Build Validation policy already exists: $pipelineName."
     }
 }
