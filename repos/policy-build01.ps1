02d3451517314bde6e91d38ebe1fd0bdc3a999ea
Karol <93782725+petitess@users.noreply.github.com>
2023-10-26 14:25:10 +0200

    create a build validation policy

--- repos/policy-build01.ps1
+++ repos/policy-build01.ps1
@@ -1,13 +1,24 @@
-$repoId = az repos list --query "[?name=='ssg-infra'].id" -o tsv
-$buildId = az pipelines build definition list --query "[?name=='infra-spoke - CI'].id" -o tsv
+$repoName = 'xxx-infra'
+$repoId = az repos list --query "[?name=='$repoName'].id" -o tsv
+$pipelineName = 'infra-spoke - CI'
+$buildId = az pipelines build definition list --query "[?name=='$pipelineName'].id" -o tsv
 
-az repos policy build create --blocking true `
-                             --branch "refs/heads/main" `
-                             --build-definition-id $buildId `
-                             --display-name "infra - CI" `
-                             --enabled true `
-                             --manual-queue-only false `
-                             --queue-on-source-update-only false `
-                             --repository-id $repoId `
-                             --valid-duration "0.0" `
-                             --path-filter "/ci/ci.yml;/iac/*"
+
+$policyValidation = az repos policy list --query "[?settings.displayName=='$pipelineName']" -o tsv
+
+if ($null -eq $policyValidation) {
+    Write-Output "Creating Build Validation policy."
+    az repos policy build create --blocking true `
+        --branch "refs/heads/main" `
+        --build-definition-id $buildId `
+        --display-name $pipelineName `
+        --enabled true `
+        --manual-queue-only false `
+        --queue-on-source-update-only false `
+        --repository-id $repoId `
+        --valid-duration "0.0" `
+        --path-filter "/ci/ci.yml;/iac/*"
+}
+else {
+    Write-Output "Build Validation policy already exists."
+}
