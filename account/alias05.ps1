30006a79b39de4058f5fb489df4cfd67d89b93fd
Karol <93782725+petitess@users.noreply.github.com>
2023-10-30 10:27:31 +0100

    create sub alias + service connection + federated credentials v3

---
+++ account/alias05.ps1
@@ -0,0 +1,116 @@
+param (
+    [Parameter(Mandatory)]
+    [String]$sub,
+
+    [Parameter(Mandatory)]
+    [String]$devopsProjectName
+)
+
+$billingAccounts = 'xxxxx-8228-43a1-acf8-32cb5b4c5d92:b5b6fef4-e666-4fd2-b303-xxxx'
+$billingProfile = 'xxxx-U5HS-BG7-xxx'
+$invoiceSections = 'xxxx-QLVW-PJA-xxx'
+$billingScope = "/providers/Microsoft.Billing/billingAccounts/$billingAccounts/billingProfiles/$billingProfile/invoiceSections/$invoiceSections"
+$rbac = "Owner"
+$devopsOrg = "https://dev.azure.com/xxxx"
+$devopsOrgName = "xxxx"
+$projectId = az devops project show --project $devopsProjectName --query "id" --output tsv
+$tenantID = az account show --query "tenantId" -o tsv
+$issuer = "https://vstoken.dev.azure.com/xxxx-823d-4a0d-9191-xxxxx"
+$token = az account get-access-token --query accessToken --output tsv
+
+#Create subscriptions
+if ($sub -match "-prod-") {
+    az account alias create --name $sub --billing-scope $billingScope --display-name $sub --workload 'Production' -o table
+}
+else {
+    az account alias create --name $sub --billing-scope $billingScope --display-name $sub --workload 'DevTest' -o table
+}
+
+$spiname = ($sub).Replace('sub', 'sp')
+
+# Create Service Principal
+$appId = az ad app create --display-name $spiname --query "appId" -o tsv
+
+if (!(az ad sp show --id $appId)) {
+az ad sp create --id $appId
+}
+# Assign the Service Principal, "Contributor" RBAC on Subscription Level:-
+$newSub = az account alias show --name $sub --query "properties.subscriptionId" -o tsv
+az role assignment create --assignee "$appId" --role "$rbac" --scope "/subscriptions/$newSub" -o table
+
+# Set Default DevOps Organisation and Project:-
+az devops configure --defaults organization=$devopsOrg project=$devopsProjectName -o table
+
+#Add subscriptions to management group
+az account management-group subscription add --name "mg-landingzones-01" --subscription $newSub -o table
+az account management-group subscription show-sub-under-mg --name "mg-landingzones-01" --query "[?displayName=='$sub'].{subName: displayName, mgName:'mg-landingzones-01'}" -o table 
+
+$app = az ad app federated-credential list --id $appId --query "[?name=='devops'].id" -o tsv
+
+if ($app) {
+    Write-Output "Federated credentials exist"
+}
+else {
+    az ad app federated-credential create --id $appId --parameters `
+        "{\""name\"": \""devops\"", \""issuer\"": \""$issuer\"", \""subject\"": \""sc://$devopsOrgName/$devopsProjectName/$spiname\"", \""audiences\"": [\""api://AzureADTokenExchange\""]}" -o table
+}
+
+# Update DevOps Service Connection with Federated Credentials:-
+$serviceEndpointId = az devops service-endpoint list --org $devopsOrg --project $devopsProjectName --query "[?name=='$spiname'].id" -o tsv
+$authorization = az devops service-endpoint list --org $devopsOrg --project $devopsProjectName --query "[?name=='$spiname'].authorization.scheme" -o tsv
+if ($serviceEndpointId) {
+    Write-Output "Found service endpoint: $serviceEndpointId"
+    if ($authorization -notmatch "WorkloadIdentityFederation") {
+        Write-Output "Configuring Workload Identity Federation"
+        $uri = "https://dev.azure.com/$devopsOrgName/$devopsProjectName/_apis/serviceendpoint/endpoints/${serviceEndpointId}?operation=ConvertAuthenticationScheme&api-version=7.2-preview.4"
+        $body = ConvertTo-Json -Depth 10 @{
+            type                             = "azurerm"
+            authorization                    = @{
+                scheme = "WorkloadIdentityFederation"
+            }
+            serviceEndpointProjectReferences = @(
+                @{
+                    name             = $spiname
+                    projectReference = @{
+                        id   = $projectId
+                        name = $devopsProjectName
+                    }
+                }
+            )
+        }
+        Invoke-RestMethod  -Method PUT -Uri $uri  -Headers @{ Authorization = "Bearer $token"; "Content-Type" = "application/json" } -Body $body
+    }
+}
+else {
+    # Create DevOps Service Connection with Federated Credentials:-
+    Write-Output "Creating service endpoint"
+    $uri = "https://dev.azure.com/$devopsOrgName/$devopsProjectName/_apis/serviceendpoint/endpoints?api-version=7.1-preview.4"
+    $body = ConvertTo-Json -Depth 10 @{
+        type                             = "azurerm"
+        name                             = $spiname
+        authorization                    = @{
+            scheme     = "WorkloadIdentityFederation"
+            parameters = @{
+                tenantid           = $tenantID
+                serviceprincipalid = $appId #$spiID
+            }
+        }
+        data                             = @{
+            subscriptionId   = $newSub
+            subscriptionName = $sub
+            environment      = "AzureCloud"
+            scopeLevel       = "Subscription"
+        }
+        serviceEndpointProjectReferences = @(
+            @{
+                name             = $spiname
+                projectReference = @{
+                    id   = $projectId
+                    name = $devopsProjectName
+                }
+            }
+        )
+    }
+    
+    Invoke-RestMethod  -Method POST -Uri $uri -Headers @{ Authorization = "Bearer $token"; "Content-Type" = "application/json" } -Body $body
+}
