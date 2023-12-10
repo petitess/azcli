173b738d6fe8ab106d42ae14a381319574664224
Karol <93782725+petitess@users.noreply.github.com>
2023-12-10 16:11:07 +0100

    create runbooks from files

--- automation/runbook01.ps1
+++ automation/runbook01.ps1
@@ -1,7 +1,9 @@
 Set-Location $PSScriptRoot
 
-$Timestamp = Get-Date -Format "dd-MMMM-yyyy HH:mm"
-$SubId = "2d9f44ea-e3df-4ea1-b956-8c7a43b119a0"
+$Date = Get-Date -Format "yyyy-MM-dd"
+$Temp = (Get-date).AddDays(1)
+$Tomorrow = Get-Date $Temp -Format "yyyy-MM-dd"
+$SubId = "xxxx-e3df-4ea1-b956-8c7a43b119a0"
 $Rg = 'rg-aa-prod-01'
 $Aa = "aa-prod-01"
 
@@ -28,3 +30,12 @@ $Runbooks | ForEach-Object {
         --resource-group $Rg `
         --subscription $SubId
 }
+
+az automation schedule create `
+    --automation-account-name $Aa `
+    --frequency "Day" `
+    --interval 1 `
+    --start-time (((Get-Date) -gt ($Date+" 20:50")) ? ($Tomorrow +" 21:00") : ($Date+" 21:00")) `
+    --name "sch-01" `
+    --resource-group $Rg `
+    --time-zone "Europe/Stockholm"
