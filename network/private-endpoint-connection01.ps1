b57765ebb7031f025b9e122a2204f3cb3589c6f2
Karol <93782725+petitess@users.noreply.github.com>
2023-12-19 10:59:41 +0100

    approve private endpoint connections

--- network/private-endpoint-connection01.ps1
+++ network/private-endpoint-connection01.ps1
@@ -1,7 +1,9 @@
 az network private-endpoint-connection list -n 'stinfraspokedevwe01' -g 'rg-infra-st-dev-we-01' --type "Microsoft.Storage/storageAccounts"
-
+####
 $StId = az storage account list --query "[].id" -o tsv
-$StId | ForEach-Object {
-    $PepId = az network private-endpoint-connection list --id $_ --query "[?properties.privateLinkServiceConnectionState.status=='Pending'].id"
+$AppId = az webapp list --query "[].id" -o tsv
+
+$StId + $AppId | ForEach-Object {
+    $PepId = az network private-endpoint-connection list --id $_ --query "[?properties.privateLinkServiceConnectionState.status=='Pending'].id" -o tsv
     az network private-endpoint-connection approve --id $PepId
 }
