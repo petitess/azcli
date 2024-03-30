7fadd7b271fc89547c016656f89086c080f3a624
Karol <93782725+petitess@users.noreply.github.com>
2024-03-30 14:15:17 +0100

    create role one line

--- role/definition_create02.ps1
+++ role/definition_create02.ps1
@@ -1,3 +1,4 @@
 az role definition create --role-definition  '{\"Name\":\"Role Y\",\"IsCustom\":true,\"Description\":\"Role X\",\"Actions\":[\"*/read\",\"Microsoft.Authorization/*\"],\"NotActions\":[],\"DataActions\":[],\"NotDataActions\":[],\"AssignableScopes\":[\"/subscriptions/2x9f44ea-e3df-4ea1-b956-8c7a43b119a0\"]}'
 az role definition create --role-definition ('{"Name":"Role Y","IsCustom":true,"Description":"Role X","Actions":["*/read","Microsoft.Authorization/*"],"NotActions":[],"DataActions":[],"NotDataActions":[],"AssignableScopes":["/subscriptions/2x9f44ea-e3df-4ea1-b956-8c7a43b119a0"]}' | ConvertTo-Json)
 az role definition delete --name "Role Y"
+az role assignment create --role "Role Y " --scope /subscriptions/2x9f44ea-e3df-4ea1-b956-8c7a43b119a0 --assignee-object-id xxxx
