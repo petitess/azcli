d0a4c5d547c07df284ab523ad0eb9056815e6dc1
Karol <93782725+petitess@users.noreply.github.com>
2026-01-10 10:57:49 +0100

    assign API permission to enterprise app

--- rest/appRoleAssignments01.ps1
+++ rest/appRoleAssignments01.ps1
@@ -1,11 +1,10 @@
 $accessToken = az account get-access-token --resource https://graph.microsoft.com/ --query accessToken -o tsv
-$spObjectId = "6bd0c4a1-6983-4640-9e20-6d1a344b78cf"
+$spObjectId = az rest --method get --uri "https://graph.microsoft.com/v1.0/servicePrincipals?`$filter=displayName eq 'func-python'" --query "value[].id" -o tsv
+$appRoleId = az rest --method get --uri "https://graph.microsoft.com/v1.0/servicePrincipals?`$filter=appId eq '00000003-0000-0000-c000-000000000000'" --query "value[].appRoles[?value=='Mail.Send'].id" -o tsv
+$mGraphId = az rest --method get --uri "https://graph.microsoft.com/v1.0/servicePrincipals?`$filter=appId eq '00000003-0000-0000-c000-000000000000'" --query "value[].id" -o tsv
 $url = "https://graph.microsoft.com/v1.0/servicePrincipals/$spObjectId/appRoleAssignments"
 $headers = "Content-type=application/json"
 # az rest --method get --uri $url
-$spObjectId = az rest --method get --uri "https://graph.microsoft.com/v1.0/servicePrincipals?`$filter=displayName eq 'sp-mg-tenant-root-group'" --query "value[].id" -o tsv
-$appRoleId = az rest --method get --uri "https://graph.microsoft.com/v1.0/servicePrincipals?`$filter=appId eq '00000003-0000-0000-c000-000000000000'" --query "value[].appRoles[?value=='Mail.Send'].id" -o tsv
-$mGraphId = az rest --method get --uri "https://graph.microsoft.com/v1.0/servicePrincipals?`$filter=appId eq '00000003-0000-0000-c000-000000000000'" --query "value[].id" -o tsv
 
 $body = ConvertTo-Json -Depth 10  @{
     "principalId" = $spObjectId
