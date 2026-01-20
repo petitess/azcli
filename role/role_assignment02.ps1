70481fc8b5b96a0b290554c89e4ab03db67adacf
Karol <93782725+petitess@users.noreply.github.com>
2026-01-20 11:58:23 +0100

    role assigment for multiple subscriptions

---
+++ role/role_assignment02.ps1
@@ -0,0 +1,28 @@
+$VmCloudockitObjectId = "abc"
+
+$Subscription = az account list --query "[?contains(name,'-prod-') && name!='sub-platform-prod-01' && state=='Enabled']" | ConvertFrom-Json
+$Subscription[0] | ForEach-Object {
+    az account set --subscription $_.id
+    $SubName = $_.name
+
+    $Reader = az role assignment list --assignee-object-id $VmCloudockitObjectId --query "[?roleDefinitionName=='Reader' && scope=='/subscriptions/$($_.id)']" | ConvertFrom-Json | ForEach-Object {
+        Write-Output "Reader role already exists: $($SubName)"
+    }
+    $Reader
+
+    if ($Reader.Count -eq 0) {
+        Write-Output "Assigning Reader role: $($SubName)"
+
+        az role assignment create --assignee $VmCloudockitObjectId --role "Reader" --scope "/subscriptions/$($_.id)"
+    }
+
+    $BillingReader = az role assignment list --assignee-object-id $VmCloudockitObjectId --query "[?roleDefinitionName=='Billing Reader' && scope=='/subscriptions/$($_.id)']" | ConvertFrom-Json | ForEach-Object {
+        Write-Output "Billing Reader role already exists: $($SubName)"
+    }
+    $BillingReader
+
+    if ($BillingReader.Count -eq 0) {
+        Write-Output "Assigning Billing Reader role: $($SubName)"
+        az role assignment create --assignee $VmCloudockitObjectId --role "Billing Reader" --scope "/subscriptions/$($_.id)"
+    }
+}
